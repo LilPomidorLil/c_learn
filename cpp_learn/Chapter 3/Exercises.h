05474d242f16c9e26b9367f9e4dee14b617cd1e2
LilPomidorLil <rozhinthe@ya.ru>
2022-04-28 14:02:04 +0300

    добавлены задания 2, 3, 4, 5 главы 3

--- cpp_learn/Chapter 3/Exercises.h
+++ cpp_learn/Chapter 3/Exercises.h
@@ -54,3 +54,103 @@ void task_1()
 		std::cout << std::endl;
 	}
 }
+
+void celcius(float& user_request)
+{
+	user_request *= 1.8;
+	user_request += 32;
+}
+
+void fahrenheit(float& user_request)
+{
+	user_request -= 32;
+	user_request /= 1.8;
+}
+
+void task_2()
+{
+	float user_request;
+	char user_answer;
+	std::cout << "Enter C - Celsius.\nEnter F - Fahrenheit." << std::endl;
+	std::cin >> user_answer;
+
+	switch (user_answer)
+	{
+	case 'C':
+		std::cout << "Enter temperature: ";
+		std::cin >> user_request;
+		celcius(user_request);
+		std::cout << "Temperature in Fahrenheit = " << user_request << std::endl;
+		break;
+
+	case 'F':
+		std::cout << "Enter temperature: ";
+		std::cin >> user_request;
+		fahrenheit(user_request);
+		std::cout << "Temperature in Celcius = " << user_request << std::endl;
+		break;
+
+	default:
+		std::cout << "Non-defined answer. Check input data and restart programm." << std::endl;
+	}
+}
+
+void task_3()
+{
+	std::cout << "Enter a number: ";
+	char ch;
+	unsigned long answer = 0;
+
+	while ((ch = _getche()) != '\r') answer = answer * 10 + ch - '0';
+	std::cout << "It turned out to be a number: " << answer << std::endl;
+}
+
+void switch_result(float& first, float& second, char& dummy)
+{
+	switch (dummy)
+	{
+	case '+':
+		std::cout << first + second;
+		break;
+
+	case '-':
+		std::cout << first - second;
+		break;
+
+	case '*':
+		std::cout << first * second;
+		break;
+
+	case '/':
+		std::cout << first / second;
+		break;
+	default:
+		std::cout << "Non-defined operand";
+		break;
+	}
+}
+
+void task_4()
+{
+	float first, second;
+	char dummy;
+	char user_answer;
+
+	do
+	{
+		std::cout << "Enter request: ";
+		std::cin >> first >> dummy >> second;
+		switch_result(first, second, dummy);
+		std::cout << "\nContinue? [y/n]: ";
+		std::cin >> user_answer;
+	} while (user_answer != 'n');
+}
+
+void task_5()
+{
+	for (int i = 0; i < 20; i++)
+	{
+		std::cout << std::setw(i) << "" << std::setfill('X') << std::endl;
+
+	}
+}
