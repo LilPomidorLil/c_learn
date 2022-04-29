fe1bb36e0bb61a0c045ac670c7c04f0ef08ef0c5
LilPomidorLil <rozhinthe@ya.ru>
2022-04-29 17:57:20 +0300

    задание 1 глава 4

---
+++ cpp_learn/Chapter 4/Exercises.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+# include <iostream>
+
+
+/// <summary>
+/// Структура для задания 1
+/// </summary>
+struct phone
+{
+	int city;
+	int phone_station;
+	int phone_abonent;
+
+	phone() : 
+		city(7), phone_station(999), phone_abonent(5716679) {}
+	
+	phone(int city_, int phone_station_, int phone_abonent_) :
+		city(city_), phone_station(phone_station_), phone_abonent(phone_abonent_) {}
+
+	friend std::istream& operator >> (std::istream& input, phone& struct_)
+	{
+		std::cout << "Enter the country code, city and other part:" << std::endl;
+		input >> struct_.city >> struct_.phone_station >> struct_.phone_abonent;
+		return input;
+	}
+
+	friend std::ostream& operator << (std::ostream& output, phone& struct_)
+	{
+		output << "My phone is -> +7-999-5716679" << std::endl;
+		output << "Your phone is -> +" << struct_.city << "-" << struct_.phone_station << "-" << struct_.phone_abonent << std::endl;
+		return output;
+	}
+};
---
+++ cpp_learn/Chapter4.h
@@ -0,0 +1,3 @@
+#pragma once
+
+# include "Chapter 4/Exercises.h"
