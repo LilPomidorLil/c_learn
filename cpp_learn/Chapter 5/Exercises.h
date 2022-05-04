8e71248c4dcece8c967058803399993dd4a4166a
LilPomidorLil <rozhinthe@ya.ru>
2022-05-04 23:36:39 +0300

    задание 3

--- cpp_learn/Chapter 5/Exercises.h
+++ cpp_learn/Chapter 5/Exercises.h
@@ -30,7 +30,8 @@
 
 /// <summary>
 /// Задание 2. 
-/// Возведение в степень
+/// Возведение в степень.
+/// Если степень четная, то делим ее на два, иначе вычитаем единицу.
 /// </summary>
 /// <param name="n"> - число типа дабл</param>
 /// <param name="p"> - в какую степень</param>
@@ -54,4 +55,29 @@ double power(double n, unsigned long long p)
 	}
 
 	return result;
+}
+
+
+/// <summary>
+/// Задание 3.
+/// Наименьшему числу присвоить нуль по ссылке. 
+/// Если оба равны между собой, то оба нуля.
+/// </summary>
+/// <param name="first"> - ссылка на первое число типа инт</param>
+/// <param name="second"> - ссылка на второе число типа инт</param>
+void zeroSmaller(int& first, int& second)
+{
+	if (first < second)
+	{
+		first = 0;
+	}
+	else if (first > second)
+	{
+		second = 0;
+	}
+	else
+	{
+		first = 0;
+		second = 0;
+	}
 }
