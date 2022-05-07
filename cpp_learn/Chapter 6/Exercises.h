40c6e6f7b5cae5b08b538c615ea6f7e2c65689ed
LilPomidorLil <rozhinthe@ya.ru>
2022-05-07 13:27:17 +0300

    ++задача 1

--- cpp_learn/Chapter 6/Exercises.h
+++ cpp_learn/Chapter 6/Exercises.h
@@ -1,3 +1,42 @@
-#pragma once
+﻿#pragma once
 
 # include <iostream>
+
+/// <summary>
+/// Задача 1. 
+/// Класс, имитирующий поведение стандартного типа инт с перегрузкой операторов.
+/// </summary>
+class Int
+{
+private:
+	int a;
+
+public:
+	Int() : a(0) {}
+	
+	Int(int A) : a(A) {}
+
+	friend std::istream& operator >> (std::istream& input, Int& obj)
+	{
+		input >> obj.a;
+		return input;
+	}
+
+	friend std::ostream& operator << (std::ostream& output, Int& obj)
+	{
+		output << obj.a;
+		return output;
+	}
+
+	friend Int operator + (Int& first, Int& second)
+	{
+		Int third(first.a + second.a);
+		return third;
+	}
+
+	friend Int operator - (Int& first, Int& second)
+	{
+		Int third(first.a - second.a);
+		return third;
+	}
+};
