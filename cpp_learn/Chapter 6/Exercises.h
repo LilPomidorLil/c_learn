14c019e88e2fc137f537ca84468ff157e9a7df20
LilPomidorLil <rozhinthe@ya.ru>
2022-05-07 13:55:03 +0300

    ++задание 2

--- cpp_learn/Chapter 6/Exercises.h
+++ cpp_learn/Chapter 6/Exercises.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 # include <iostream>
+# include <conio.h>  // для getche()
 
 /// <summary>
 /// Задача 1. 
@@ -38,5 +39,63 @@ public:
 	{
 		Int third(first.a - second.a);
 		return third;
+	}
+};
+
+class toolBooth
+{
+private:
+	unsigned int total_car;
+	double total_sum;
+
+public:
+	toolBooth() : total_car(0), total_sum(0) {}
+
+	void payingCar()
+	{
+		total_car++;
+		total_sum += 0.5;
+	}
+
+	void nopayCar()
+	{
+		total_car++;
+	}
+
+	void display() const
+	{
+		int not_pay_car_total = 2 * (total_car * 0.5 - total_sum);
+		std::cout << "Total car = " << total_car << std::endl << "Total sum = " << total_sum << std::endl << "Not payed " << not_pay_car_total;
+	}
+
+	void run()
+	{
+		char ch;
+		do
+		{
+			std::cout << "'Y' if ~payingCar~, else 'N'" << std::endl;
+
+			std::cin >> ch;
+
+			switch (ch)
+			{
+			case 'Y':
+				this->payingCar();
+				break;
+
+			case 'N':
+				this->nopayCar();
+				break;
+
+			case 'S':
+				break;
+
+			default:
+				std::cout << "I can't understand!" << std::endl;
+				break;
+			}
+		} while (ch != 'S');
+
+		this->display();
 	}
 };
